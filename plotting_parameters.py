2b67386305735b80549ea4f24466c3466b7b3dfe
hartenbergma <martin.hartenberger@ookonzept.de>
2024-02-07 23:00:01 +0100

    plots

--- plotting_parameters.py
+++ plotting_parameters.py
@@ -5,7 +5,7 @@ import spectral as sp
 import os
 import sys
 from preprocessing import get_array, smooth_spectral
-
+import matplotlib.colors as mcolors
 
 tum_blue_brand ='#3070b3'
 tum_blue_dark = '#072140' #
@@ -97,7 +97,7 @@ def plot_spectrum(spectr, wavelengths, fig, ax, legend=False, nspectr=None):
 
 def plot_class_spectra(img, gt_map, nspectr=None, bands=None, figsize=(18,5)):
     '''
-    Plot the spectra of the pixels in the classes Normal, Tumor, and Hypervasculized of the ground truth map.
+    Plot the spectra of the pixels in the classes Normal, Tumor, and Hypervascularized of the ground truth map.
     input:
         img: spectral.ImageArray or np.array containing the image
         gt_map: spectral.ImageArray or np.array containing the ground truth map
@@ -117,7 +117,7 @@ def plot_class_spectra(img, gt_map, nspectr=None, bands=None, figsize=(18,5)):
     img = get_array(img)
     gt_map = gt_map.asarray()
 
-    class_labels = ["Not labled", "Normal", "Tumor", "Hypervasculized", "Background"]
+    class_labels = ["Not labled", "Normal", "Tumor", "Hypervascularized", "Background"]
     class_ids = [1, 2, 3]
     fig, axs = plt.subplots(1, len(class_ids), figsize=figsize)
     # plot pixel spectra for each class
@@ -133,7 +133,8 @@ def plot_class_spectra(img, gt_map, nspectr=None, bands=None, figsize=(18,5)):
 
 def plot_bands(img, bands=[200, 300, 400, 500, 600, 700], figsize=(18,3)):
     '''
-    Plot selected bands of the image as gray-scale images.
+    Plot selected bands of the image as gray-scale images. 
+    The contrast is increased by setting the maximum value to the 99.9th percentile.
     input:
         img: spectral.ImageArray or np.array containing the image
         bands: list of bands to plot
@@ -145,14 +146,18 @@ def plot_bands(img, bands=[200, 300, 400, 500, 600, 700], figsize=(18,3)):
     img = get_array(img)
     fig, axs = plt.subplots(1, len(bands), figsize=figsize)
     for i, band in enumerate(bands):
-        axs[i].imshow(img[:,:,band], cmap='gray', aspect='equal')
+        img_band = img[:,:,band]
+        img_band = (img_band - np.min(img_band)) / (np.max(img_band) - np.min(img_band))
+        vmax = np.percentile(img_band, 99.9)
+        axs[i].imshow(img_band, cmap='gray', aspect='equal', vmin=0, vmax=vmax)
         axs[i].set_title(f'Band {band}')
         axs[i].axis('off')
     return fig, axs
 
 def get_rgb(img, bands=[109,192,425]):
     '''
-    Convert the image to RGB using the specified bands
+    Convert the image to RGB using the specified bands.
+    The contrast is increased by setting the maximum value to the 99.9th percentile.
     input:
         img: image to convert, SpyFile or array-like
         bands: bands to use for RGB, list of ints
@@ -164,7 +169,8 @@ def get_rgb(img, bands=[109,192,425]):
             bands = [int(band) for band in img.metadata["default bands"]]
     img = get_array(img)
     img_rgb = np.stack([img[:,:,bands[2]], img[:,:,bands[1]], img[:,:,bands[0]]], axis=-1).squeeze()
-    img_rgb = (img_rgb - img_rgb.min(axis=(0,1)))/ (img_rgb.max(axis=(0,1)) - img_rgb.min(axis=(0,1)))
+    img_rgb = (img_rgb - img_rgb.min(axis=(0,1)))/ (np.percentile(img_rgb, 99.9) - img_rgb.min(axis=(0,1)))
+    img_rgb = np.clip(img_rgb, 0, 1)
     return img_rgb
 
 def plot_img(img, gt_map=None, class_labels=None, class_colors=None, bands=[109,192,425], figsize=(5,5), legend=True):
@@ -181,19 +187,22 @@ def plot_img(img, gt_map=None, class_labels=None, class_colors=None, bands=[109,
     '''
     img_rgb = get_rgb(img, bands=bands)
 
-    fig, ax = plt.subplots(figsize=figsize)
-    ax.imshow(img_rgb)
+    fig, ax = plt.subplots()
     if gt_map is not None:
         gt_map = gt_map.asarray()
         class_ids = np.unique(gt_map)
-        if class_labels is None:
-            raise ValueError("class_labels must be provided when gt_map is provided")
+        if class_labels is None or class_colors is None:
+            raise ValueError("class_labels and class_colors must be provided when gt_map is provided")
         for class_id in class_ids[class_ids != 0]:
             mask = np.where(gt_map[:,:,0] == class_id)
-            if class_colors is None:
-                ax.scatter(mask[1], mask[0], marker='.', s=1, label=class_labels[class_id])
-            else:
-                ax.scatter(mask[1], mask[0], marker='.', s=1, label=class_labels[class_id], color=class_colors[class_id])
+            overlay = np.zeros_like(gt_map[:,:,0])
+            overlay[mask] = 1
+            overlay = np.repeat(overlay[:, :, np.newaxis], 3, axis=2)
+            color = mcolors.hex2color(class_colors[class_id])
+            img_rgb = np.where(overlay,
+                        np.array(color),
+                        img_rgb)
+    ax.imshow(img_rgb, aspect='equal', vmin=0, vmax=np.percentile(img_rgb, 50))
     ax.axis('off')
     if legend:
         ax.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0., markerscale=10)
@@ -230,5 +239,8 @@ def plot_class_dist(img, gt_map, bands, class_ids, class_labels, class_colors, f
     ax.set_xlabel('Wavelength (nm)')
     if legend_loc is not None:
         # ax.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
-        ax.legend(loc=legend_loc, handlelength=0.8)
+        order = [0,2,1]
+        handles, labels = plt.gca().get_legend_handles_labels()
+        ax.legend([handles[idx] for idx in order],[labels[idx] for idx in order], loc=legend_loc, handlelength=0.8, borderaxespad=0.)
+        # ax.legend(loc=legend_loc, handlelength=0.8, borderaxespad=0.)
     return fig, ax
